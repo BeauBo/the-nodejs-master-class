6c16533494d13e96dfadd5b17ebb2c1b0a5772b2
Bo Huang <bo.huang@paytm.com>
2018-12-20 12:55:47 -0500

    homework-assignment

--- src/index.js
+++ src/index.js
@@ -1,101 +1,63 @@
-/**
- * Primary file for the API
- */
-
-// Dependencies
-
 const http = require('http');
 const https = require('https');
+const fs = require('fs');
 const url = require('url');
 const { StringDecoder } = require('string_decoder');
-const fs = require('fs');
 const config = require('./config');
 
-// instantiate the HTTP server
-const httpServer = http.createServer((req, res) => {
-  unifiedServer(req, res);
-});
 
-// Start the HTTP server
-httpServer.listen(config.httpPort, () => console.log(`The server is listening on port ${config.httpPort} now`));
+// Instantiate HTTP server
+const httpServer = http.createServer((req, res) => server(req, res));
 
-// Instantiate the HTTPS server
-const httpsServerOption = {
+// Start HTTP server
+httpServer.listen(config.httpPort, () => console.log(`Server is now listening on port: ${config.httpPort}`));
+
+
+// Instatiate HTTPS server
+const httpsOptions = {
   key: fs.readFileSync('../https/key.pem'),
   cert: fs.readFileSync('../https/cert.pem'),
 };
-const httpsServer = https.createServer(httpsServerOption, (req, res) => {
-  unifiedServer(req, res);
-});
-// Start the HTTPS server
-httpsServer.listen(config.httpsPort, () => console.log(`The server is listening on port ${config.httpsPort} now`));
-// All the server logic for both http and https server
-const unifiedServer = (req, res) => {
-  // Get the URL and parse it
-  const parseURL = url.parse(req.url, true);
+const httpsServer = https.createServer(httpsOptions, (req, res) => server(req, res));
+// Start HTTPS server
+httpsServer.listen(config.httpsPort, () => console.log(`Server is now listening port: ${config.httpsPort}`));
 
-  // Get the path
-  const path = parseURL.pathname;
-  const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+// Server for both HTTP and HTTPS
+const server = ((req, res) => {
+  const parsedURL = url.parse(req.url, true);
+  const path = parsedURL.pathname.replace(/^\/+|\/+$/g, '').toLowerCase();
 
-  // Get the query string as an object
-  const queryStringObject = parseURL.query;
-
-  // Get HTTP method
-  const method = req.method.toLowerCase();
-
-  // Get the headers as an object
-  const { headers } = req;
-
-  // Get the payload, if any
+  // Get payload
   const decoder = new StringDecoder('utf-8');
   let buffer = '';
+
   req.on('data', data => buffer += decoder.write(data));
   req.on('end', () => {
-    buffer += decoder.end();
-
-    // Choose the handler this request should go to. If one is not found, use the not found handler
-    const chosenHandler = typeof router[trimmedPath] !== 'undefined' ? router[trimmedPath] : handlers.notFound;
-
-    // Construct the data object to send to the handler
-    const data = {
-      trimmedPath,
-      queryStringObject,
-      method,
-      headers,
-      payload: buffer,
-    };
-
-    // Route the request to the handler specified in the router
-    chosenHandler(data, (statusCodeCalledBack, payloadCalledBack) => {
-      // Use the status code called back by the handler, or default to 200
+    const routerHandler = typeof routers[path] === 'undefined' ? handlers.notFound : routers[path];
+    routerHandler(buffer, (statusCodeCalledBack, payloadCalledBack) => {
       const statusCode = typeof statusCodeCalledBack === 'number' ? statusCodeCalledBack : 200;
-      // Use the payload called back by the handler, or default an empty obejct
       const payload = typeof payloadCalledBack === 'object' ? payloadCalledBack : {};
-      // Convert the payload to a string
-      const payloadString = JSON.stringify(payload);
+      const payloadString = JSON.stringify(`${payload.message}, the data you just posted is '${payload.data}'`);
 
-      // Return the response
       res.setHeader('Content-Type', 'application/json');
       res.writeHead(statusCode);
-      res.end(payloadString);
-      // Log the request path
-      console.log('Returning this response:', statusCode, payloadString);
+
+      if (statusCode === 404) {
+        res.end('No records found');
+      } else {
+        res.end(payloadString);
+      }
+      console.log('Returning this response:', statusCode, payload);
     });
   });
+});
+
+// Handlers
+const handlers = {
+  notFound: (data, cb) => cb(404),
+  hello: (data, cb) => cb(200, { data, message: 'Welcome to HELLO party' }),
 };
 
-// Define handlers
-const handlers = {};
-
-// Ping handler
-handlers.ping = (data, cb) => {
-  cb(200);
-};
-
-// Not found handler
-handlers.notFound = (data, cb) => cb(404);
-// Define a request router
-const router = {
-  ping: handlers.ping,
+const routers = {
+  hello: handlers.hello,
 };
