0de6d402b7020e6adb0c95f9f926c61df762ddb6
Bo Huang <bo.huang@paytm.com>
2019-01-02 10:03:22 -0500

    Connecting to an API

--- src/config.js
+++ src/config.js
@@ -13,6 +13,11 @@ env.staging = {
   envName: 'staging',
   hashingSecret: 'thisIsASecret',
   maxChecks: 5,
+  twilio: {
+    accountSid: 'ACb32d411ad7fe886aac54c665d25e5c5d',
+    authToken: '9455e3eb3109edc12e3d8c92768f7a67',
+    fromPhone: '+15005550006',
+  },
 };
 
 // Production env
